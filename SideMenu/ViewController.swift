e7f3db9f0a03a0d2cee4a6844b4bf48023b96410
Anokhi <anokhias@gmail.com>
2023-10-28 20:49:02 +0200

    Initial Commit

---
+++ SideMenu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SideMenu
+//
+//  Created by Anokhi Shah on 28.10.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
