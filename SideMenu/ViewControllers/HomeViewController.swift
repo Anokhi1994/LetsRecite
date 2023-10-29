ac4b08b552082e0ff91b912715349f33a708fb07
Anokhi <anokhias@gmail.com>
2023-10-29 14:45:02 +0100

    added basic app template with sideMenu

---
+++ SideMenu/ViewControllers/HomeViewController.swift
@@ -0,0 +1,32 @@
+//
+//  HomeViewController.swift
+//  SideMenu
+//
+//  Created by Anokhi Shah on 28.10.23.
+//
+
+import UIKit
+
+protocol HomeViewControllerDelegate: AnyObject {
+    func didTapMenuButton()
+}
+
+class HomeViewController: UIViewController {
+    
+    weak var delegate: HomeViewControllerDelegate?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        title = "Home"
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "list.dash"), style: .done, target: self, action: #selector(didTapMenuButton))
+        
+    }
+    
+    @objc func didTapMenuButton(){
+        delegate?.didTapMenuButton()
+    }
+
+
+}
